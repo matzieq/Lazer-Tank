b3d9efd3be3310e234e2c6d4975c16aaa563b0e5
Maciej Kozlowski <mjp.kozlowski@gmail.com>
2018-12-11 18:46:07 +0100

    Add terrain

--- src/states/game.js
+++ src/states/game.js
@@ -7,7 +7,10 @@ LazerTank.Game.prototype = {
         this.load.spritesheet('PlayerTank', "./img/PlayerTank.png", 16, 16, 3);
         this.load.image('Bullet', "./img/Bullet.png");
         this.load.image('Brick', "./img/Bricks.png");
+        this.load.image('Tiles', "./img/TankTerrain.png");
+
         this.load.tilemap('brickmap', './map/bricksForTank.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.tilemap('terrainLevel1', './map/terrainLevel1.json', null, Phaser.Tilemap.TILED_JSON);
 
         this.load.audio('enginehi', './snd/engine.wav');
         this.load.audio('enginelo', './snd/enginelow.wav');
@@ -19,20 +22,25 @@ LazerTank.Game.prototype = {
         this.enableCrispRendering();
         
         console.log(this.game);
-        this.tank = new LazerTank.PlayerTank(this.game, 100, 100);
         console.log(this.tank);
         this.cursors = this.input.keyboard.createCursorKeys();
-
+        
         this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
+        
         this.TANK_VELOCITY = 100;
         this.TANK_ANIMATION_SPEED = 20;
+        
+        this.tank = new LazerTank.PlayerTank(this.game, 100, 100);
 
-        var game = this.game;
-        this.map = this.add.tilemap('brickmap');
+        // this.map = this.add.tilemap('brickmap');
+        this.map = this.add.tilemap('terrainLevel1');
         console.log(this.map);
-        this.map.addTilesetImage('bricks', 'Brick');
-        this.layer = this.map.createLayer('bricks');
+        // this.map.addTilesetImage('bricks', 'Brick');
+        this.map.addTilesetImage('TankTerrain', 'Tiles');
+        // this.layer = this.map.createLayer('bricks');
+        this.metalLayer = this.map.createLayer('metal');
+        this.waterLayer = this.map.createLayer('water');
+        this.bushLayer = this.map.createLayer('bushes');
         console.log(this.layer);
         this.layer.resizeWorld();
 
